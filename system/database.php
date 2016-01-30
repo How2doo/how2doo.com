bdedfc380badb2782ee3fe80868e8e4e6cc720fa
Savepong <microsave@live.com>
2016-01-30 19:28:15 +0700

    Set up instructure

---
+++ system/database.php
@@ -0,0 +1,11 @@
+<?php
+$connect = mysqli_connect("database_host","database_username","database_password","database");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+mysqli_set_charset($connect,"utf8");
+?>
---
+++ system/setting.php
@@ -0,0 +1,18 @@
+<?php
+//Site
+$site_name="Inspires Studio";
+$site_title="Inspires Studio";
+$site_address="http://localhost/";
+$site_domain="jmaxglobal.com";
+$site_url="$site_address"."$site_domain";
+$site_dir="$site_url/site";
+$site_system="$site_url/system";
+$site_homepage="test";
+
+//Security
+$private_key="";
+$timeout=3600;
+
+//Timezone
+date_default_timezone_set("Asia/Bangkok");
+?>
